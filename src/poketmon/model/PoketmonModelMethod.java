b163d6562500cb74014380c645ed086efe9172cb
Gino <Gino@LAPTOP-J5SJL6II>
2019-07-14 16:30:42 +0900

    0714 DS-02

--- src/poketmon/model/PoketmonModelMethod.java
+++ src/poketmon/model/PoketmonModelMethod.java
@@ -1,5 +1,24 @@
 package poketmon.model;
 
+import poketmon.exception.NotExistException;
+import poketmon.model.dto.Poketmon;
+import poketmon.model.dto.Trainer;
+
 public class PoketmonModelMethod {
 
+	
+	public void dataUpdate(String trainerName, Object obj) throws NotExistException{
+		Trainer trainer = getTrainer(trainerName);
+		if(trainer == null) {
+			throw new NotExistException("xxxxxxx 수정하고자 하는 Trainer가 미 존재합니다. xxxxxxx\n");
+		}
+		if(obj instanceof String) {
+			trainer.setName((String)obj);
+		} else if(obj instanceof Poketmon) {
+			trainer.setPoketmon((Poketmon)obj);
+		} else {
+			trainer.setBadge((Integer.parseInt((String)obj)));			// 입력받은 값을 정수 타입으로 변환
+		}
+		
+	}
 }
--- src/proketmonView/StartView.java
+++ src/proketmonView/StartView.java
@@ -6,10 +6,13 @@ public class StartView {
 	public static void main(String[] args) {
 		PoketmonModelDB poket = PoketmonModelDB.getInstance();
 		
-		
-				
+		// 모든 트레이너 정보 출력
+		System.out.println("모든 트레이너 정보 출력");
 		System.out.println(poket.getTrainer());
-//		System.out.println(poket.a);
+
+		// 새로운 포켓몬
+		
+		// 새로운 트레이너
 	}
 }
 
