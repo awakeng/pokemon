d29198636fd7183f588eb85e5c0b6ae97a7c113d
Gino <Gino@LAPTOP-J5SJL6II>
2019-07-14 17:33:40 +0900

    0714 DS--06

--- src/poketmon/controller/PoketmonController.java
+++ src/poketmon/controller/PoketmonController.java
@@ -1,6 +1,11 @@
 package poketmon.controller;
 
+import javax.security.auth.login.FailedLoginException;
+
+import poketmon.exception.NotExistException;
 import poketmon.model.PoketmonModelMethod;
+import poketmon.model.dto.Trainer;
+import proketmonView.FailView;
 
 public class PoketmonController {
 	private static PoketmonController instance = new PoketmonController();
@@ -16,8 +21,28 @@ public class PoketmonController {
 	// 특정 프로젝트 검색
 	
 	// 새로운 프로젝트 저장
+	public void insertTrainer(Trainer newTrainer) {
+		method.trainerInsert(newTrainer);
+	}
 	
 	// 존재하는 프로젝트 수정
+	public void updateProject(String trainerName, Object obj) {
+		try {
+			method.dataUpdate(trainerName, obj);
+		} catch (NotExistException e) {
+			FailView.failMessageView(e.getMessage());
+		}
+	}
 	
 	// 모든 프로젝트 삭제
+	public void deleteTrainer(String trainerName) {
+		try {
+			method.trainerDelete(trainerName);
+		} catch (NotExistException e) {
+			FailView.failMessageView(e.getMessage());
+			//e.printStackTrace();
+		}
+	}
 }
+
+
---
+++ src/proketmonView/FailView.java
@@ -0,0 +1,7 @@
+package proketmonView;
+
+public class FailView {
+	public static void failMessageView(String message) {
+		System.out.println(message);
+	}
+}
